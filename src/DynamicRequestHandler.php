7e0e29878b9e09c667e7ffb33067246f7d2c28d0
ImDushan <ineshmadhushan@gmail.com>
2024-03-25 12:52:57 +0400

    push file to repository

---
+++ src/DynamicRequestHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modularize\Core;
+
+use InvalidArgumentException;
+
+class DynamicRequestHandler
+{
+    /**
+     * Get a value from the request, or throw an exception if not present.
+     *
+     * @param array  $request  The request array.
+     * @param string $key      The key to retrieve from the request.
+     * @param mixed  $default  The default value if the key is not present.
+     *
+     * @return mixed
+     *
+     * @throws InvalidArgumentException
+     */
+    public static function getRequestValue(array $request, string $key, $default = null)
+    {
+        if (array_key_exists($key, $request)) {
+            return $request[$key];
+        }
+
+        if ($default !== null) {
+            return $default;
+        }
+
+        throw new InvalidArgumentException("Missing required attribute: {$key}");
+    }
+}
